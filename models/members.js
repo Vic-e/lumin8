a113d5dda9364065449aebc2e82790dcffa625a4
Victoria Coker <vic-e@Victorias-MacBook-Air.local>
2022-08-24 08:38:07 -0400

    Added member schema

---
+++ models/members.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema ({
+name: { type: String, required: true },
+title: { type: String, required: true },
+img: String,
+bio: String,
+accomplishments: String,
+contact: [String],
+location: String,
+tags: [String]
+})
+
+const Member = mongoose.model('Member', memberSchema);
+
+module.exports = Member;
